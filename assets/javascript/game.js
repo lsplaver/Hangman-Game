f7221e6f8428af3f7f717dd8f60eec59a80942b1
unknown <lsplaver@yahoo.com>
2018-02-15 16:24:01 -0500

    Update 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,10 @@ const LETTER_ARRAY = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
 var randNumber = Math.floor(Math.random() * 26);
 
 // variable holding the game's chosen winning letter
-var winningLetter = letterArray[randNumber];
+var winningLetter = LETTER_ARRAY[randNumber];
 
 // variable holding remaining player guesses
+var defaultRemainingGuesses = 10;
 var remainingGuesses = 10;
 
 // variable holding current number of player wins
@@ -20,5 +21,72 @@ var playerTotalLosses = 0;
 var playerGuesses = [];
 
 // sets the playerGuesses to length of 10
-playerGuesses.length(10);
+// playerGuesses.length = 10;
+
+console.log("LETTER_ARRAY values (index: 0 - 25): " + LETTER_ARRAY.toString());
+
+console.log("randNumber value: " + randNumber);
+
+console.log("winningLetter value: " + winningLetter);
+
+console.log("defaultRemainingGuesses value : " + defaultRemainingGuesses);
+
+console.log("remainingGuesses value: " + remainingGuesses);
+
+console.log("playerTotalWins value: " + playerTotalWins);
+
+console.log("playerTotalLosses value: " + playerTotalLosses);
+
+console.log("playerGuesses.length value: " + playerGuesses.length.toString());
+
+console.log("playerGuesses array value: " + playerGuesses.toString());
+
+var main = document.getElementById("gameBody");
+
+main.onload = initLayout();
+
+// function newDivFunction(var elemID, variable 
+
+function initLayout() {
+    main.setAttribute("onKeyUp", "guess()");
+    var idLosses = document.getElementById("playerLosses");
+    
+    var idRemainingPlayerGuesses = document.getElementById("remainingPlayerGuesses");
+    
+    var idCurrentGuesses = document.getElementById("playerCurrentGameGuesses");
+
+    // var newDiv = document.createElement("div")
+
+    // main.appendChild(newDiv);
+
+    // newDiv = document.createElement("div")
+    // var x = "";
+    // x = newDiv.getAttribute("id");
+    // console.log("the current id value of newDiv is: " + x);
+    // newDiv.setAttribute("id", "playerWins");
+
+    // var igTies;
+    // igTies = document.getElementById("playerWins");
+    var idWins = document.getElementById("playerWins");
+    var idPlayWin2 = document.getElementById("playerWins2");
+    idPlayWin2.textContent = "Wins: ";
+    // idWins.textContent = "Wins: "; // + playerTotalWins.toString();
+
+    idLosses.textContent = "Losses: "; // + playerTotalLosses.toString();
+
+    idRemainingPlayerGuesses.textContent = "Guesses Left: "; // + remainingGuesses.toString();
+
+    idCurrentGuesses.textContent = "Your Guesses So Far: "; // + playerGuesses.toString();
+}
+var x = "";
+
+// if (!(main.onkeyup == null)) {
+    x = main.onclick; // .addEventListener("keyup", guess());
+    guess();
+// }
+
+function guess() {
+    console.log("A key was pressed!!! \nThe key pressed was: " + x);
+    x = "";
+}
 
